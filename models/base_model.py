ef6671a63cd02c0de929b3de5c0b1c19fd17b3d4
Amira Walid <amirawaleed012@gmail.com>
2024-06-19 16:27:02 +0300

    base model

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from datetime import datetime, timezone
+import uuid
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class BaseModel:
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
+    
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(key, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs.keys():
+                self.created_at = datetime.now(timezone.utc)
+            if "updated_at" not in kwargs.keys():
+                self.updated_at = datetime.now(timezone.utc)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now(timezone.utc)
+            self.updated_at = datetime.now(timezone.utc)
+    
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+   
+    def to_dict(self):
+        """Convert instance into dict format"""
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
+        return my_dict
+
