0acc27faf3db8377c68e25a7068e7f5806173947
Amira Walid <amirawaleed012@gmail.com>
2024-06-22 15:51:54 +0300

    test

---
+++ models/test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3 
+from base_model import BaseModel
+from user import User
+from post import Post
+from comment import Comment
+from like import Like
+
+amira = User(name='amira', password='123', email='amira@email.com')
+esraa = User(name='esraa', password='1234', email='esraa@email.com')
+
+post0 = Post(content='Hi! My name is amira.', user_id = amira.id)
+amira.posts.append(post0)
+
+comment0 = Comment(content=f'Hi Amira!!\nMy name is esraa.', user_id= esraa.id, post_id=post0.id)
+esraa.comments.append(comment0)
+post0.comments.append(comment0)
+
+print(post0.user.name)
+print(post0.comments[0].content)
+print(post0.comments[0].user.name)
+
+like0 = Like(user_id=esraa.id, post_id = post0.id)
+
+post0.likes.append(like0)
+esraa.likes.append(like0)
+
+print(post0.likes[0].user)
+
