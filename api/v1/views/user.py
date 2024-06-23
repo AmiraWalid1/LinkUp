36b2e9c8b1de266f72e2fc5711c333b7a691368e
azziimm7 <azzah31@outlook.com>
2024-06-23 09:06:24 +0000

    user view

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -21,4 +21,49 @@ def get_user(user_id):
         abort(404, "User Not Found")
     return jsonify(user.to_dict())
 
+@app_views.route('/users/<user_id>', methods=["DELETE"], strict_slashes=False)
+def delete_user(user_id):
+    """
+    delete user object
+    """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404, "User Not Found")
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
 
+
+@app_views.route('/users/<user_id>', methods=["PUT"], strict_slashes=False)
+def update_user(user_id):
+    """
+    A function to update user object
+    """
+    if not request.json:
+            abort(400, 'Not a JSON')
+    ignore = ['id', 'created_at', 'updated_at']
+    data = request.json
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404, "User Not Fount")
+    for k, v in data.items():
+        if k not in ignore:
+           setattr(user, k, v)
+           user.save()
+    return jsonify(user.to_dict()), 200
+
+
+user_attrs = ['name', 'email', 'password']
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user(user_id):
+    """
+    create user attributes
+    """
+    if not request.json:
+        abort(400, 'Not a JSON')
+    for attr in user_attrs:
+        if attr not in request.json:
+            abort(400, f'Missing <{attr}> Attribute')
+    user = User(**request.json)
+    user.save()
+    return jsonify(user.to_dict()), 201
