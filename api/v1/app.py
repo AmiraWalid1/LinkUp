241c2943f05570f9ad2f65d615c16d08821502c4
azziimm7 <azzah31@outlook.com>
2024-06-23 07:37:12 +0000

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,4 +6,25 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+
+@app.teardown_appcontext
+def storageClose(exception):
+    """ A method calls storage. close to teardown up """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ handle 404 error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    """ start main app """
+    app.run(host=host, port=port, threaded=True)
