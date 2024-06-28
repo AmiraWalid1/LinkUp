afdf55628661e61a3d07ecdba1987569ed0d99d7
azziimm7 <azzah31@outlook.com>
2024-06-28 13:43:49 +0000

    secret config

--- web_flask/app.py
+++ web_flask/app.py
@@ -4,25 +4,26 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template, url_for
-from forms import RegistrationForm, LoginForm
+from web_flask.forms import RegistrationForm, LoginForm
+
+
 app = Flask(__name__)
-
-
 app.config['SECRET_KEY'] = '125381cf7c3b4af8e4659822bff66c6f'
 
 
 @app.route('/', strict_slashes=False)
-@app.route('/login', strict_slashes=False)
+@app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login():
     """returns log in page"""
     form = LoginForm()
     return render_template('login.html', form=form)
 
-@app.route('/signUp', strict_slashes=False)
+
+@app.route('/signUp', methods=['GET', 'POST'], strict_slashes=False)
 def signup():
     """returns sign up page"""
     form = RegestrationForm()
     return render_template('signUp.html', form=form)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
--- web_flask/forms.py
+++ web_flask/forms.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
+
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, length, Email
+from wtforms.validators import DataRequired, Length, Email
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
