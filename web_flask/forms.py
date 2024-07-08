4fba4a07b35380c5f147aaa0bdd29dd9cd7ad1b5
azziimm7 <azzah31@outlook.com>
2024-07-08 19:08:38 +0000

    import TextAreaField

--- web_flask/forms.py
+++ web_flask/forms.py
@@ -2,7 +2,7 @@
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email
 
 
