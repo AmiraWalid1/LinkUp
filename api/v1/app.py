61f7fe9eaded50c1f888ef464cfc8881257d9f4d
Amira Walid <amirawaleed012@gmail.com>
2024-07-12 02:19:55 +0300

    connect signup and login to db

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 host = os.getenv('LinkUp_API_HOST', '0.0.0.0')
