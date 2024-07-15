606945040a90ab1889a341844e256b466151576a
azza <azzah31@outlook.com>
2024-07-15 17:40:34 +0200

    add script

--- web_flask/static/script/comments.js
+++ web_flask/static/script/comments.js
@@ -3,13 +3,16 @@ async function getComments(){
     let comments = await likesResponse.json();
 
     for(let comment of comments){
+        let userResponse = await fetch(`http://localhost:5000/api/v1/posts/${comment.user_id}`);
+        let user = await userResponse.json();
 
         let postResponse = await fetch(`http://localhost:5000/api/v1/posts/${comment.post_id}`);
         let post = await postResponse.json();
 
-        let userResponse = await fetch(`http://localhost:5000/api/v1/users/${post.user_id}`);
-        let user = await userResponse.json()
-        console.log(user);
-        document.querySelector('.posts-container').innerHTML += getHTML(user, comment);
+        let authorResponse = await fetch(`http://localhost:5000/api/v1/users/${post.user_id}`);
+        let author = await authorResponse.json()
+        
+        document.querySelector('.posts-container').innerHTML += getHTML(post, author, user, comment);
     }
 }
+getComments();
