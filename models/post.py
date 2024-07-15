c5ad63ecfd8cf05146925ed32754983e1f7a1847
Amira Walid <amirawaleed012@gmail.com>
2024-07-15 15:40:31 +0300

    add more column in my models

--- models/post.py
+++ models/post.py
@@ -7,6 +7,7 @@ from models.base_model import BaseModel, Base
 class Post(BaseModel, Base):
     __tablename__ = "posts"
     content = Column(String(1000), nullable=False)
+    photo = Column(String(1000), nullable=True)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     user = relationship('User', back_populates='posts')
--- models/user.py
+++ models/user.py
@@ -6,10 +6,15 @@ from flask_login import UserMixin
 
 class User(BaseModel, Base, UserMixin):
     __tablename__ = "users"
-    name = Column(String(60), nullable=False)
-    password = Column(String(30), nullable=False, unique=True)
+    first_name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    username = Column(String(60), nullable=False)
+    password = Column(String(30), nullable=False)
     email = Column(String(30), nullable=False, unique=True)
-    bio = Column(String(100), nullable=True)
+    bio = Column(String(100), nullable=False)
+    country = Column(String(30), nullable=False)
+    city = Column(String(30), nullable=False)
+    website = Column(String(255), nullable=True) 
     photo = Column(String(255), nullable=True)  # column for storing photo path or URL
 
     posts = relationship("Post", back_populates="user")
