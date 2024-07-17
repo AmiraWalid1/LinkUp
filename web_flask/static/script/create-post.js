a0e8b2070b6247a5b70a49b418809ded66d96c1d
Amira Walid <amirawaleed012@gmail.com>
2024-07-17 16:48:25 +0300

    add posts_user

--- web_flask/static/script/create-post.js
+++ web_flask/static/script/create-post.js
@@ -6,33 +6,9 @@ let user_img = document.querySelector(".post-pic");
 user_name.innerHTML = user.first_name + ' ' + user.last_name + user_name.innerHTML;
 user_img.src = user.photo || "../static/styles/imgs/me.png";
 
-// document.getElementById('file').addEventListener('change', function(event) {
-//     const fileInput = event.target;
-//     const file = fileInput.files[0];
-
-//     if (file) {
-//         const reader = new FileReader();
-
-//         reader.onload = function(e) {
-//             const imageContainer = document.getElementById('selected-image-container');
-//             const imageElement = document.getElementById('selected-image');
-            
-//             imageElement.src = e.target.result;
-//             imageElement.style.display = 'block'; // Show the image
-//             console.log(e.target.result);
-//             // Optional: Add the selected image to your post data or perform other actions
-//             console.log('Selected Image:', file);
-//         };
-
-//         reader.readAsDataURL(file);
-//     }
-// });
-
-document.querySelector(".Posts").addEventListener("click", async function (event){
+document.querySelector(".Posts").addEventListener("click", async function (event) {
     event.preventDefault();
-    
+
     const content = document.querySelector('#postContent').value;
-    // const photo = event.target.dataset.image;
-    
     await postPost(user, {content});
 });
